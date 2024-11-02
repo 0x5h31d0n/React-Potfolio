3bf8e5b8c5d216f9bd20458156e86345a99976ec
Sheldon Menezes <samymenezes33@gmail.com>
2024-11-02 19:26:40 +0530

    fix: lottie animations compressed

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Lottie from 'lottie-react';
-import { Mail, Phone,Github } from 'lucide-react';
+import { Mail, Phone,Github,Linkedin } from 'lucide-react';
 import contact from '../assets/contact.json';
 import '../style.css';
 
 const contactDetails = {
   email: 'samymenezes33@gmail.com',
   phno: '+91 8618387647',
-  github: '0x5h31d0n'
+  github: '0x5h31d0n',
+  linkedin: 'sheldonangelomenezes',
 };
 
 function Contact() {
@@ -17,6 +18,13 @@ function Contact() {
         <h2 className='top'>Contact Me</h2>
         <Lottie animationData={contact} loop={true} className="contact-animation" />
         <div className="contact-grid">
+          <a href={`https://linkedin.com/in/${contactDetails.linkedin}/`} className='contact-item'>
+            <div className="contact-content">
+              <Linkedin size={24} />
+              <h3>Linkedin</h3>
+              <span className='contact-detail'>{contactDetails.linkedin}</span>
+            </div>
+          </a>
           <a href={`https://github.com/${contactDetails.github}`} className='contact-item'>
             <div className="contact-content">
               <Github size={24} />
@@ -29,13 +37,6 @@ function Contact() {
               <Mail size={24} />
               <h3>Email</h3>
               <span className='contact-detail'>{contactDetails.email}</span>
-            </div>
-          </a>
-          <a href={`tel:${contactDetails.phno}`} className='contact-item'>
-            <div className="contact-content">
-              <Phone size={24} />
-              <h3>Phone</h3>
-              <span className='contact-detail'>{contactDetails.phno}</span>
             </div>
           </a>
           <a href={`tel:${contactDetails.phno}`} className='contact-item'>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Lottie from 'lottie-react';
+import React, { Suspense } from 'react';
 import { ReactTyped } from "react-typed";
 import skate from '../assets/skate.json';
-import '../style.css'; // Assuming you have a specific CSS file for the Hero component
+import '../style.css';
+
+const LazyLottie = React.lazy(() => import('lottie-react'));
 
 function Hero() {
   return (
     <section id="hero">
       <div className="hero-content">
         <div className="hero-text">
-          <h1>Hello There👋🏻, <br></br>Im <a className="name" href='/'>Sheldon Menezes</a></h1>
+          <h1>Hello There👋🏻, <br />Im <a className="name" href='/'>Sheldon Menezes</a></h1>
           <h2>
             <ReactTyped
               strings={['developer', 'Infosec enthusiast', 'CTF player', 'bug bounty hunter']}
@@ -20,7 +21,9 @@ function Hero() {
           </h2>
         </div>
         <div className="hero-animation">
-          <Lottie animationData={skate} loop={true} className="animation" style={{ width: '300px', height: '300px' }} />
+          <Suspense fallback={<div>Loading Animation...</div>}>
+            <LazyLottie animationData={skate} loop={true} className="animation" style={{ width: '300px', height: '300px' }} />
+          </Suspense>
         </div>
       </div>
     </section>
