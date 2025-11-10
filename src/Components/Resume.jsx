02750e60802d48748540be08882b9ab72e673140
Sheldon Menezes <samymenezes33@gmail.com>
2025-11-10 14:39:09 +0530

    fix: education section fixed and resume updated

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -6,7 +6,7 @@ const resume = () => {
     <section className='main-resume'>
     <h2 className='top'>Resume</h2>
     <div className='resume-container'>
-        <iframe src="https://drive.google.com/file/d/1PaGPYXdZtvVWfIxz40BaJmBkKrvWUBGY/preview" 
+        <iframe src="https://drive.google.com/file/d/1M2KuIyuZjjxVf2IwiNcfuYh8kgh-sm0m/preview" 
         allow="autoplay" className='resume'>
         </iframe>
     </div>
